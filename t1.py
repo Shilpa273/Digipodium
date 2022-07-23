3ed4d00a18e1ae0c4de7a76de078466c5b165ffa
Shilpa Prajapati <108970962+Shilpaprajapati02@users.noreply.github.com>
2022-07-23 22:34:14 +0530

    control

--- t1.py
+++ t1.py
@@ -6,4 +6,4 @@ left(90)
 forward(200)
 left(90)
 forward(100)
-mainloop() #stop the window from closing
+mainloop() # stop the window from closing
---
+++ t2.py
@@ -0,0 +1,24 @@
+from turtle import*
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+left(36)
+forward(80)
+
+
+mainloop()
+
