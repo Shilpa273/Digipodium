97bf8f1ba0b4abcb5c3ca36ec3c3fa325f667b00
Shilpa Prajapati <108970962+Shilpaprajapati02@users.noreply.github.com>
2022-07-23 12:15:09 +0530

    control

---
+++ t1.py
@@ -0,0 +1,9 @@
+from turtle import*
+forward(200) #move forward 200 units
+left(90) #turn left 90 degrees
+forward(100)
+left(90)
+forward(200)
+left(90)
+forward(100)
+mainloop() #stop the window from closing
